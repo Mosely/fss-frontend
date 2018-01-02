c774af64a960a32248a19f397cc0f11e6de26e72
James Lee Baker <jamesleebaker@Jamess-Mac-Pro.local>
2018-01-01 23:31:29 -0600

    Updating user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,15 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  created_at: DS.attr('date'),
+  createdAt: DS.attr('date'),
   email: DS.attr('string'),
-  id: DS.attr('number'),
-  is_disabled: DS.attr('boolean'),
+  isDisabled: DS.attr('boolean'),
   password: DS.attr('string'),
   password2: DS.attr('string'),
-  password_created_at: DS.attr('date'),
-  updated_at: DS.attr('date'),
-  updated_by: DS.attr('number'),
-  username: DS.attr('string')
+  passwordCreatedAt: DS.attr('date'),
+  updatedAt: DS.attr('date'),
+  username: DS.attr('string'),
+
+  // Relationships
+  updatedBy: DS.belongsTo('person')
 });
