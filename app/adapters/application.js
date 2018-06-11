3c8d553acb8660196c581208aebdcc21504e2abb
Mosely <djhiggs1@gmail.com>
2018-06-11 11:51:44 -0500

    Going Back to RESTAdapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,12 +6,14 @@ const {
   inject: { service }
 } = Ember;
 
-//export default DS.RESTAdapter.extend(DataAdapterMixin, {
-  export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+export default DS.RESTAdapter.extend(DataAdapterMixin, {
+  //export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+
   host: `${config.host}`,
   session: service('session'),
   authorize(xhr) {
     let { access_token } = this.get('session.data.authenticated');
+    
     xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
     debugger;
   }
