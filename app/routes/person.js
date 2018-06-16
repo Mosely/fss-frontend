40d4c0d532ebdc6695094f3f300cea32c3dff5a1
Mosely <djhiggs1@gmail.com>
2018-06-15 21:25:14 -0500

    Reverting Array Notation

--- app/routes/person.js
+++ app/routes/person.js
@@ -4,7 +4,7 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Route.extend(AuthenticatedRouteMixin, {
   // NOTE: May ahve to deserialize the returned date for it to display
   model(params) {
-    return this.store.findRecord('person', params.id).data[0];
+    return this.store.findRecord('person', params.id);
     //this.store.query('person', { filter: { id: params.id } }).then(function (person) {
     //  return person.get('firstObject');
     //});
--- app/routes/user.js
+++ app/routes/user.js
@@ -4,7 +4,7 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Route.extend(AuthenticatedRouteMixin, {
     // NOTE: May ahve to deserialize the returned date for it to display
     model(params) {
-        return this.store.findRecord('user', params.id).data[0];
+        return this.store.findRecord('user', params.id);
         //this.store.query('user', { filter: { id: params.id } }).then(function (user) {
         //    return user.get('firstObject');
         //});
