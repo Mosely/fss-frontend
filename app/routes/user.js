3780262e65a0f0662bfb780addf787c8e692ad07
Mosely <djhiggs1@gmail.com>
2018-06-15 21:02:45 -0500

    Really Fixing teh Array to Object Issue

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,8 +2,11 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  // NOTE: May ahve to deserialize the returned date for it to display
-  model(params) {
-    return this.store.findRecord('user', params.id)[0];
-  }
+    // NOTE: May ahve to deserialize the returned date for it to display
+    model(params) {
+        //return this.store.findRecord('user', params.id)[0];
+        this.store.query('user', { filter: { id: params.id } }).then(function (user) {
+            return user.get('firstObject');
+        });
+    }
 });
