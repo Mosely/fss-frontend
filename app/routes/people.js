9daeba8728e45b7b70175ecdc8aba8e37cf38753
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-06-20 23:56:05 -0500

    removing comments/trying to return users data in single route

--- app/routes/people.js
+++ app/routes/people.js
@@ -2,8 +2,15 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  // NOTE: May ahve to deserialize the returned date for it to display
   model() {
-    return this.store.findAll('person');
+    let store = this.store;
+    return this.store.findAll('person').then(function(people) {
+      people.forEach(person => {
+        store.findRecord('user', person.id).then(function(user) {
+          person.set('user', user);
+        });
+      });
+      return people;
+    });
   }
 });
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,16 +1,7 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import RSVP from 'rsvp';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  // NOTE: May ahve to deserialize the returned date for it to display
-   /*model() {
-     return this.store.findAll('user').then(function (users) {
-       return users.forEach((user) => {
-         user.get('person');
-       });
-     });
-   },*/
    model() {
     let store = this.store;
     return store.findAll('user').then(function(users){
@@ -20,44 +11,6 @@ export default Route.extend(AuthenticatedRouteMixin, {
         });
       });
       return users;
-      /*return Ember.RSVP.all(users.getEach('person')).then(function(){
-        users.forEach(user => {
-          let person = user.person;
-          console.log("user id = " + user.id + " and firstName = " + person.firstName);
-        });
-        return users;
-      });*/
     });
    }
-   //model() {
-   //  return this.store.findAll('user');
-   //}
-  //model() {
-  //  return RSVP.hash({
-  //    users: this.store.findAll('user'),
-  //    people: this.store.findAll('person'),
-  //  });
-  //}
-  // afterModel(model, transition) {
-  //   this._super(controller, model);
-  //   return RSVP.all(model.getEach('people'));
-  // },
-
-  // setupController(controller, model) {
-  //   this._super(controller, model);
-  //   controller.set('users', model.users);
-  //   controller.set('people', model.people);
-  // },
-
-  /*model() {
-    var promise = new Ember.RSVP.Promise(function (resolve, reject) {
-      this.store.findAll('user').then(function (users) {
-        var personPromises = users.map(function(s){ return s.get('person') });
-        Ember.RSVP.all(personPromises).then(function () {
-          resolve(users);
-        });
-      });
-    });
-    return promise;
- }*/
 });
