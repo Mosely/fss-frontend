9daff936828d613cfa9b79daa40ac14380f4ee6e
Mosely <djhiggs1@gmail.com>
2018-06-21 18:42:16 -0500

    Figuring Out Custom Inflectors

--- app/models/custom-inflector-rules.js
+++ app/models/custom-inflector-rules.js
@@ -3,7 +3,7 @@ import Inflector from 'ember-inflector';
 const inflector = Inflector.inflector;
 
 // Tell the inflector that the plural of "campus" is "campuses"
-inflector.irregular('branch-of-service', 'branchesofservice');
+inflector.irregular('branchofservice', 'branchesofservice');
 
 // Tell the inflector that the plural of "advice" is "advice"
 //inflector.uncountable('advice');
