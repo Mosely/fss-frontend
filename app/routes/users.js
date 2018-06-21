fe98671eb5b639cf3ce749eeec9bbc56d316784a
Mosely <djhiggs1@gmail.com>
2018-06-20 20:21:42 -0500

    Forcing Person Model Retrieval

--- app/routes/users.js
+++ app/routes/users.js
@@ -17,6 +17,7 @@ export default Route.extend(AuthenticatedRouteMixin, {
       users.forEach(user => {
         store.findRecord('person', user.id).then(function(person) {
           user.set('person', person);
+          user.get('person');
           console.log("user id = " + user.id + " and firstName = " + person.firstName);
         });
       });
