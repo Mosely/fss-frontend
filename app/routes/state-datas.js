3a7712e06c05e56e166834149b79fb85b01098e3
Mosely <djhiggs1@gmail.com>
2018-06-21 18:02:19 -0500

    Added Remaining Resource Routes

---
+++ app/routes/state-datas.js
@@ -0,0 +1,17 @@
+import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Route.extend(AuthenticatedRouteMixin, {
+   model() {
+    let store = this.store;
+    return store.findAll('state-data').then(function(primaryModels){
+      // Modify the following to pull in any related models as needed
+      //primaryModels.forEach(primaryModel => {
+      //  store.findRecord('REPLACEWITHRELATEDMODEL', primaryModel.id).then(function(secondaryModel) {
+      //    primaryModel.set('REPLACEWITHRELATEDMODEL', secondaryModel);
+      //  });
+      //});
+      return primaryModels;
+    });
+   }
+});
