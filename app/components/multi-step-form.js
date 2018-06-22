c5be722f952462054dfba69b10360497d7a4898a
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-06-21 20:52:45 -0500

    removing old action for multi step form

--- app/components/multi-step-form.js
+++ app/components/multi-step-form.js
@@ -3,23 +3,23 @@ import Component from '@ember/component';
 export default Component.extend({
 
 	actions: {
-		multiStep() {
-			let item = document.querySelectorAll('.form-step--item');
-			let i = 0;
-
-			item[i].classList.remove('active');
-			item[i].classList.add('hidden');
-			i++;
-
-			if (i > item.length) {
-				i = 0;
-			}
-
-			item[i].classList.remove('hidden');
-			item[i].classList.add('active');
-
-			console.log(i);
-		},
+		// multiStep() {
+		// 	let item = document.querySelectorAll('.form-step--item');
+		// 	let i = 0;
+		//
+		// 	item[i].classList.remove('active');
+		// 	item[i].classList.add('hidden');
+		// 	i++;
+		//
+		// 	if (i > item.length) {
+		// 		i = 0;
+		// 	}
+		//
+		// 	item[i].classList.remove('hidden');
+		// 	item[i].classList.add('active');
+		//
+		// 	console.log(i);
+		// },
 	}
 
 });
