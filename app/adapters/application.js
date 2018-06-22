3a8ba5e9eff5c95d486cbc00b7046d6b0e1036a7
Mosely <djhiggs1@gmail.com>
2018-06-21 19:25:03 -0500

    Probably No Need to Override PathForType Anymore

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,16 +2,16 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import { inject as service } from '@ember/service';
 import config from '../config/environment';
-import { pluralize } from 'ember-inflector';
+//import { pluralize } from 'ember-inflector';
 
 //export default DS.RESTAdapter.extend(DataAdapterMixin, {
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
 
   host: `${config.host}`,
-  pathForType: function(type) {
-    let typeCleaned = type.replace(/-/g,'');
-    return pluralize(typeCleaned);
-  },
+  //pathForType: function(type) {
+  //  let typeCleaned = type.replace(/-/g,'');
+  //  return pluralize(typeCleaned);
+  //},
   session: service('session'),
   authorize(xhr) {
     let { access_token } = this.get('session.data.authenticated');
