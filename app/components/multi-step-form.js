7ec257a5e6cb9902c3f34047af5889dce0f69f76
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-06-22 11:47:30 -0500

    Making multi-step-form component to actuall work

--- app/components/multi-step-form.js
+++ app/components/multi-step-form.js
@@ -1,27 +1,28 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+	currentItem: 0,
+
+	showItem(num) {
+		let item = document.querySelectorAll('.form-step--item');
+		//take the param and add block display to the specified item
+		item[num].classList.remove('hidden');
+		item[num].classList.add('active');
+	},
 
 	actions: {
-
-		showItem(num) {
-			let item = document.querySelectorAll('.form-step--item');
-			//take the param and add block display to the specified item
-			item[num].classList.add('active');
-		},
-
 		nextPrev() {
-			let currentItem = 0;
 			let item = document.querySelectorAll('.form-step--item');
 			//make the current item display none
-			item[currentItem].classList.add('hidden');
-			item[currentItem].classList.remove('active');
+			item[this.currentItem].classList.add('hidden');
+			item[this.currentItem].classList.remove('active');
 			// increase item index by one
-			currentItem = currentItem + 1;
+			this.currentItem += 1;
 
-			if (currentItem >= item.length) {
+			if (this.currentItem >= item.length) {
 				return false;
 			}
+			return this.showItem(this.currentItem);
 		},
 	}
 
