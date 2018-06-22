67c136dd94c82310a28bb936b6cc36484704646c
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-06-21 21:01:02 -0500

    radio button color change

--- app/components/multi-step-form.js
+++ app/components/multi-step-form.js
@@ -3,23 +3,25 @@ import Component from '@ember/component';
 export default Component.extend({
 
 	actions: {
-		// multiStep() {
-		// 	let item = document.querySelectorAll('.form-step--item');
-		// 	let i = 0;
-		//
-		// 	item[i].classList.remove('active');
-		// 	item[i].classList.add('hidden');
-		// 	i++;
-		//
-		// 	if (i > item.length) {
-		// 		i = 0;
-		// 	}
-		//
-		// 	item[i].classList.remove('hidden');
-		// 	item[i].classList.add('active');
-		//
-		// 	console.log(i);
-		// },
+		currentItem: 0,
+
+		nextPrev() {
+			// let item = document.querySelectorAll('.form-step--item');
+			// let i = 0;
+			//
+			// item[i].classList.remove('active');
+			// item[i].classList.add('hidden');
+			// i++;
+			//
+			// if (i > item.length) {
+			// 	i = 0;
+			// }
+			//
+			// item[i].classList.remove('hidden');
+			// item[i].classList.add('active');
+			//
+			// console.log(i);
+		},
 	}
 
 });
