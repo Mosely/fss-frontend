461bcd25f2ca16c6c125e4e26e0badd2f57a0107
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-06-23 00:30:39 -0500

    removing current user functions in route

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,20 +3,5 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 import { inject as service } from '@ember/service';
 
 export default Route.extend(ApplicationRouteMixin, {
-	session: service('session'),
-  currentUser: service('current-user'),
-	
-	beforeModel(transition) {
-		this._loadCurrentUser();
-	},
-
-	sessionAuthenticated() {
-		this._super(...argutments);
-		this._loadCurrentUser();
-	},
-
-	_loadCurrentUser() {
-		return this.get('currentUser').loadUser().cath(() => this.get('session').invalidate());
-	}
 
 });
--- app/routes/user.js
+++ app/routes/user.js
@@ -14,5 +14,21 @@ export default Route.extend(AuthenticatedRouteMixin, {
         //this.store.query('user', { filter: { id: params.id } }).then(function (user) {
         //    return user.get('firstObject');
         //});
+    },
+    session: service('session'),
+    currentUser: service(),
+
+    beforeModel() {
+      this._loadCurrentUser();
+    },
+
+    sessionAuthenticated() {
+      this._super(...argutments);
+      this._loadCurrentUser();
+    },
+
+    _loadCurrentUser() {
+      console.log("test");
+      return this.get('currentUser').loadUser().cath(() => this.get('session').invalidate());
     }
 });
