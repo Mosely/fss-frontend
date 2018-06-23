96d4f8fa6c8e5af0114f81a5d63790efb83be474
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-06-22 23:50:05 -0500

    Editing multi-step-form component action to handle prev/next and submit

--- app/components/multi-step-form.js
+++ app/components/multi-step-form.js
@@ -5,21 +5,36 @@ export default Component.extend({
 
 	showItem(num) {
 		let item = document.querySelectorAll('.form-step--item');
+		let next = document.getElementById("next");
+		let previous = document.getElementById("prev");
 		//take the param and add block display to the specified item
 		item[num].classList.remove('hidden');
 		item[num].classList.add('active');
+		// display or hide the previous button
+		if (num == 0) {
+			previous.style.display = "none";
+		} else {
+			previous.style.display = "inline-block";
+		}
+		if (num == (item.length -1) ) {
+			next.innerHTML = "Submit";
+		} else {
+			next.innerHTML = "Next";
+		}
 	},
 
 	actions: {
 		nextPrev(x) {
 			let item = document.querySelectorAll('.form-step--item');
+			let form = document.getElementById('form');
 			//make the current item display none
 			item[this.currentItem].classList.add('hidden');
 			item[this.currentItem].classList.remove('active');
 			// increase item index by one
-			this.currentItem += 1;
+			this.currentItem += x;
 
 			if (this.currentItem >= item.length) {
+
 				return false;
 			}
 			return this.showItem(this.currentItem);
