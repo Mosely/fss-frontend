a267c20a615985f76f1916b4565937a29064d8e7
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-06-23 00:24:02 -0500

    Editing app route in attempt to load current user

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,18 +5,18 @@ import { inject as service } from '@ember/service';
 export default Route.extend(ApplicationRouteMixin, {
 	session: service('session'),
   currentUser: service('current-user'),
+	
+	beforeModel(transition) {
+		this._loadCurrentUser();
+	},
 
-  // beforeModel() {
-  //   return this._loadUser();
-  // },
+	sessionAuthenticated() {
+		this._super(...argutments);
+		this._loadCurrentUser();
+	},
 
-  sessionAuthenticated() {
-    this._loadUser().then(()=>{
-      this.transitionTo('/');
-    }).catch(() => this.session.invalidate());
-  },
+	_loadCurrentUser() {
+		return this.get('currentUser').loadUser().cath(() => this.get('session').invalidate());
+	}
 
-  _loadUser() {
-    return this.get('currentUser').loadUser();
-  }
 });
