e70003979041ad5f9ec2e7dca89f981d9114a926
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-07-03 18:58:48 -0500

    Adding formValidation function for error handling of multi-step-form

--- app/components/multi-step-form.js
+++ app/components/multi-step-form.js
@@ -1,44 +1,68 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 
 export default Component.extend({
-	currentItem: 0,
+  currentItem: 0,
 
-	showItem(num) {
-		let item = document.querySelectorAll('.form-step--item');
-		let next = document.getElementById("next");
-		let previous = document.getElementById("prev");
-		//take the param and add block display to the specified item
-		item[num].classList.remove('hidden');
-		item[num].classList.add('active');
-		// display or hide the previous button
-		if (num == 0) {
-			previous.style.display = "none";
-		} else {
-			previous.style.display = "inline-block";
-		}
-		if (num == (item.length -1) ) {
-			next.innerHTML = "Submit";
-		} else {
-			next.innerHTML = "Next";
-		}
-	},
+  showItem(num) {
+    let next,
+      previous,
+      item = document.querySelectorAll(".form-step--item");
+    (next = document.getElementById("next")),
+      (previous = document.getElementById("prev"));
+    //take the param and add block display to the specified item
+    item[num].classList.remove("hidden");
+    item[num].classList.add("active");
+    // display or hide the previous button
+    if (num == 0) {
+      previous.style.display = "none";
+    } else {
+      previous.style.display = "inline-block";
+    }
+    if (num == item.length - 1) {
+      next.innerHTML = "Submit";
+    } else {
+      next.innerHTML = "Next";
+    }
+  },
 
-	actions: {
-		nextPrev(x) {
-			let item = document.querySelectorAll('.form-step--item');
-			let form = document.getElementById('form');
-			//make the current item display none
-			item[this.currentItem].classList.add('hidden');
-			item[this.currentItem].classList.remove('active');
-			// increase item index by one
-			this.currentItem += x;
+  validateForm() {
+    var item,
+      i,
+      input,
+      valid = true;
+    item = document.querySelectorAll(".form-step--item");
+    input = item[this.currentItem].querySelectorAll("input.required");
 
-			if (this.currentItem >= item.length) {
-				form.submit();
-				return false;
-			}
-			return this.showItem(this.currentItem);
-		},
-	}
+    for (i = 0; i < input.length; i++) {
+      if (input[i].value == "") {
+        input[i].classList.add("invalid");
+				valid = false;
+      } else {
+        input[i].classList.remove("invalid");
+      }
+    }
+    return valid;
+  },
 
+  actions: {
+    nextPrev(x) {
+      let form,
+        item = document.querySelectorAll(".form-step--item");
+      form = document.getElementById("form");
+      if (x == 1 && !this.validateForm()) {
+        return false;
+      }
+      //make the current item display none
+      item[this.currentItem].classList.add("hidden");
+      item[this.currentItem].classList.remove("active");
+      // increase item index by one
+      this.currentItem += x;
+
+      if (this.currentItem >= item.length) {
+        form.submit();
+        return false;
+      }
+      this.showItem(this.currentItem);
+    }
+  }
 });
