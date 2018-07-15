45cd2e59533a21f21148fbab055105106f8373ad
Mosely <djhiggs1@gmail.com>
2018-07-14 20:10:48 -0500

    Just Some Checking

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -15,7 +15,7 @@ export default Controller.extend({
       session
         .authenticate(authenticator, credentials)
         .then(() => {
-          console.log(this.get('session'));
+          console.log(this.get('session.data.authenticated.user_id'));
           let id = this.get('session.data.authenticated.user_id');
           //this.store.queryRecord('user', {}).then((user) => {
           this.store.findRecord('user', id).then((user) => {
