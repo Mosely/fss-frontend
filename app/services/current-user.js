b4151ae3369187c3900c0e66ec7edb28cdeac356
Mosely <djhiggs1@gmail.com>
2018-07-14 21:10:20 -0500

    More Debugging

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -13,8 +13,6 @@ export default Service.extend({
     console.log(this.get('session'));
 		if (this.get('session.isAuthenticated')) {
       let id = this.get('session.data.currentUser.id');
-      console.log("The currentUSer.id: " + id);
-      debugger;
       //return this.store.findRecord('user', {id: true}).then((user) => {
       return this.store.findRecord('user', id).then((user) => {
         this.set("user", user);
