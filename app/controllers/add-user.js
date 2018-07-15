84cde18b615eb7e6357b08f76f07af406d12650e
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-07-15 13:09:54 -0500

    Changing add-user controller to hopfully create a record

--- app/controllers/add-user.js
+++ app/controllers/add-user.js
@@ -1,15 +1,25 @@
 import Controller from "@ember/controller";
-import { inject as service } from '@ember/service';
+import { inject as service } from "@ember/service";
 
 export default Controller.extend({
-  // session: service("session"),
-  //
-  // actions: {
-  //   addUser() {
-  //     let userInfo = this.getProperties('firstName', 'lastName', 'username', 'email', 'password');
-  //     let newUser = this.store.createRecord('user', userInfo);
-  //     return newUser.save();
-  //
-  //   }
-  // }
+  // injecting session service
+  session: service("session"),
+
+  actions: {
+    addUser() {
+      let store = this.store;
+			let personProps = this.getProperties('firstName', 'middleName', 'lastName', 'dateOfBirth', 'age');
+			let userProps = this.getProperties('username', 'email', 'password');
+      // let userInfo = this.getProperties('firstName', 'lastName', 'username', 'email', 'password');
+      // let newUser = this.store.createRecord('user', userInfo);
+      // return newUser.save();
+      return store.createRecord("person", personProps).then(person => {
+        store.findRecord("user", person.id).then(user => {
+          let userData = store.createRecord("user", userProps);
+          person.set("user", userData);
+        });
+        return person.save();
+      });
+    }
+  }
 });
