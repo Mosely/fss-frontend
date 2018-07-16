d52a77b9548ca5d95ef2451c2846410fa955a4c2
Mosely <djhiggs1@gmail.com>
2018-07-15 21:45:12 -0500

    Reworked User Creation to Use Component Workflow

--- app/components/add-user-form.js
+++ app/components/add-user-form.js
@@ -5,8 +5,45 @@ export default Component.extend({
   session: service("session"),
 
   actions: {
-    submit() {
-      this.sendAction('triggerSave');
+    triggerSave() 
+    //{
+    //  this.sendAction('triggerSave');
+    //}
+    {
+      let store = this.store;
+
+      let personProps = this.getProperties('firstName', 'middleName', 'lastName', 'dateOfBirth', 'age');
+      console.log(model);
+      let userProps = {
+        username: this.get("username"),
+        email: this.get("email"),
+        password: this.get("password")
+      };
+      let newPerson = store.createRecord("person", personProps);
+      let newUser = store.createRecord("user", { id: newPerson.id, userProps });
+			newPerson.save().then(() => {
+				return newUser.save();
+			});
+
+
+      // return newPerson.save().then(() => {
+      // 	newUser.save();
+      // });
+
+      // let userInfo = this.getProperties('firstName', 'lastName', 'username', 'email', 'password');
+      // let newPerson = this.store
+      //   .createRecord("person", personProps)
+      //   .then(person => {
+      //     this.store.createRecord("user", { id: person.id, userProps });
+      //   });
+      // return newUser.save();
+      // return store.createRecord("person", personProps).then(person => {
+      //   store.findRecord("user", person.id).then(user => {
+      //     let userData = store.createRecord("user", userProps);
+      //     person.set("user", userData);
+      //   });
+      //   return person.save();
+      // });
     }
   }
 });
