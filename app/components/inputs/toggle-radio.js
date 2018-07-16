8db413863a6c5078f9e9a0df9d12a4f3c11263f8
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-07-15 23:48:28 -0500

    adding more of the toggle-radio component on audlt form

--- app/components/inputs/toggle-radio.js
+++ app/components/inputs/toggle-radio.js
@@ -1,16 +1,15 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 
 export default Component.extend({
-	tagName: "div",
-	classNames: ["form-check", "inline-block"],
-	label: null,
-	idValue: null,
-	attributeBindings: ["data-toggle"],
+  tagName: "div",
+  classNames: ["form-check", "inline-block"],
+  label: null,
+  idValue: null,
+  attributeBindings: ["data-toggle"],
 
-	actions: {
-		ifYesToggle() {
+  actions: {
+    ifYesToggle() {
       let value = event.target.value;
-			console.log(value);
       let parentElem = document.getElementById(event.target.parentElement.id);
       let targetElem = document.getElementById(parentElem.dataset.toggle);
       if (value == "yes" && targetElem.classList.contains("hidden")) {
@@ -21,5 +20,5 @@ export default Component.extend({
         targetElem.classList.add("hidden");
       }
     }
-	}
+  }
 });
