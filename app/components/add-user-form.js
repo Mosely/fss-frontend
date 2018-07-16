18ed71eb506c0cbaad87b1d7a2c344d71d490379
Mosely <djhiggs1@gmail.com>
2018-07-15 22:01:44 -0500

    Addressing Scope Issue

--- app/components/add-user-form.js
+++ app/components/add-user-form.js
@@ -4,14 +4,13 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   session: service("session"),
   store: service("store"),
-  
+
   actions: {
     triggerSave() 
     //{
     //  this.sendAction('triggerSave');
     //}
     {
-      let store = this.store;
 
       let personProps = this.getProperties('firstName', 'middleName', 'lastName', 'dateOfBirth', 'age');
 
