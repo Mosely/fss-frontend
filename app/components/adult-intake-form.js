e8b1fa69de40f822588ad03ef0e9fc1518b2ddfd
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-07-15 22:13:36 -0500

    Deleting unneeded code

--- app/components/adult-intake-form.js
+++ app/components/adult-intake-form.js
@@ -1,14 +1,4 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-	actions: {
-		ifYesToggle(){
-			let elemID = document.getElementById(event.target.id);
-			console.table(elemID);
-			console.log(elemID.dataset.id);
-			// if (targetItem.classList.contains("hidden")) {
-			// 	targetItem.classList.remove("hidden");
-			// }
-		}
-	}
 });
