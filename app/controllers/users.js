4b7b00ad08eabbf3b981debb9897228903fc3df8
Mosely <djhiggs1@gmail.com>
2018-07-18 00:52:15 -0500

    Trying to Force Array Return

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -4,7 +4,7 @@ export default Controller.extend({
 	actions: {
 		filterByName(param) {
 			if (param !== '') {
-				return [ this.get('store').query('user', { username: param }) ];
+				return [ { user: this.get('store').query('user', { username: param }) } ];
 			} else {
 				return this.store.findAll('user');
 			}
