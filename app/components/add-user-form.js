260e74209696ecea7eacf00dc1756f5945e470fa
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-07-17 23:31:44 -0500

    Adding promise to genderModel

--- app/components/add-user-form.js
+++ app/components/add-user-form.js
@@ -37,34 +37,28 @@ export default Component.extend({
         "age"
       );
 
-      genderModel = store.findRecord("gender", personProps.gender);
+      genderModel = store.findRecord("gender", personProps.gender).then((gender) => {
+        console.log("GENDERID " + gender.id);
+        newPerson = store.createRecord("person", personProps);
 
-      console.table(personProps);
+        userProps = this.getProperties("username", "email", "password");
+        newUser = store.createRecord("user", {
+          id: newPerson.id,
+          userProps
+        });
 
-      userProps = this.getProperties("username", "email", "password");
-      // let newPerson = store.createRecord("person", {
-      //   firstName: this.get("firstName"),
-      //   middleName: this.get("middleName"),
-      //   lastName: this.get("lastName"),
-      //   gender_id: this.get("user.gender"),
-      //   dateOfBirth: this.get("dateOfBirth"),
-      //   age: this.get("age")
-      //  });
-      newPerson = store.createRecord("person", personProps);
-      newUser = store.createRecord("user", {
-        id: newPerson.id,
-        userProps
+        newPerson.set("gender", gender.id);
+
+        newPerson.save().then(() => {
+          return newUser.save();
+        });
       });
 
-      let resolvedPromises = Promise.all([genderModel, newPerson, newUser]).then((values)=> {
-        return values;
-      });
-      newPerson.set("gender", resolvedPromises[0]);
-      console.table(newPerson);
-      
-      newPerson.save().then(() => {
-        return newUser.save();
-      });
+      // let resolvedPromises = Promise.all([genderModel, newPerson, newUser]).then((values)=> {
+      //   return values;
+      // });
+      // newPerson.set("gender", resolvedPromises);
+      // console.log(resolvedPromises);
     }
   }
 });
