5f012211e84f632d4e56c92432f23364a75900f1
Mosely <djhiggs1@gmail.com>
2018-07-17 20:13:31 -0500

    Maybe This WIll Work

--- app/components/add-user-form.js
+++ app/components/add-user-form.js
@@ -36,29 +36,28 @@ export default Component.extend({
         "age"
       );
 
-      personProps.gender = store.findRecord("gender", personProps.gender).then(()=> {
-        console.table(personProps);
-
-        userProps = this.getProperties("username", "email", "password");
-        // let newPerson = store.createRecord("person", {
-        //   firstName: this.get("firstName"),
-        //   middleName: this.get("middleName"),
-        //   lastName: this.get("lastName"),
-        //   gender_id: this.get("user.gender"),
-        //   dateOfBirth: this.get("dateOfBirth"),
-        //   age: this.get("age")
-        //  });
-        newPerson = store.createRecord("person", personProps);
-        newUser = store.createRecord("user", {
-          id: newPerson.id,
-          userProps
-        });
-        newPerson.save().then(() => {
-          return newUser.save();
-        });
+      personProps.gender = store.findRecord("gender", personProps.gender).then((gender)=> {
+        return gender;
       });
+      console.table(personProps);
 
+      userProps = this.getProperties("username", "email", "password");
+      // let newPerson = store.createRecord("person", {
+      //   firstName: this.get("firstName"),
+      //   middleName: this.get("middleName"),
+      //   lastName: this.get("lastName"),
+      //   gender_id: this.get("user.gender"),
+      //   dateOfBirth: this.get("dateOfBirth"),
+      //   age: this.get("age")
+      //  });
+      newPerson = store.createRecord("person", personProps);
+      newUser = store.createRecord("user", {
+        id: newPerson.id,
+        userProps
+      });
+      newPerson.save().then(() => {
+        return newUser.save();
+      });
     }
-    
   }
 });
