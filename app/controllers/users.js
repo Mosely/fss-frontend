257cd2a6affef7c43fadf338fe7e4907d4024b51
Mosely <djhiggs1@gmail.com>
2018-07-19 10:21:44 -0500

    Trying ot get data attribute to be an array

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -4,7 +4,7 @@ export default Controller.extend({
 	actions: {
 		filterByName(param) {
 			if (param !== '') {
-				return [ { user: this.get('store').query('user', { username: param }) } ];
+				return { data: [ this.get('store').query('user', { username: param })['data'] ] };
 			} else {
 				return this.store.findAll('user');
 			}
