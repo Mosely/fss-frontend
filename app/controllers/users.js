f7f602e8f801a024fa753fbd3ad85a1e35a0b166
Mosely <djhiggs1@gmail.com>
2018-07-19 10:57:51 -0500

    Building New Array

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -6,7 +6,16 @@ export default Controller.extend({
 			var store = this.get('store');
 			if (param !== '') {
 				console.log("Getting ready to run the user query.");
-				return { data: [ store.query('user', { username: param }).data, {} ] };
+				let records = store.query('user', { username: param });
+				records.then((recordsArray) => {
+					if(records.data.length == 1) {
+						recordsArray.data[0] = records.data;
+						return recordsArray;
+					} else {
+						return records;
+					}
+				});
+				//return { data: [ store.query('user', { username: param }).data, {} ] };
 			} else {
 				return this.store.findAll('user');
 			}
