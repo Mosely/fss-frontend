0639d339e14018a60477f8f76a49145d4ecd7351
Mosely <djhiggs1@gmail.com>
2018-07-19 10:44:32 -0500

    One More Try

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,8 +3,9 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	actions: {
 		filterByName(param) {
+			var store = this.get('store');
 			if (param !== '') {
-				return { data: [ this.get('store').query('user', { username: param }).data ] };
+				return { data: [ store.query('user', { username: param }).data, {} ] };
 			} else {
 				return this.store.findAll('user');
 			}
