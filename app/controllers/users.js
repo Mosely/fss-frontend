caeb11f3b81f0ea6be05a557a0cfc4535d9ec33d
Mosely <djhiggs1@gmail.com>
2018-07-19 11:04:57 -0500

    More Debugging

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,11 +5,12 @@ export default Controller.extend({
 		filterByName(param) {
 			var store = this.get('store');
 			if (param !== '') {
-				console.log("Getting ready to run the user query.");
+				
 				let records = store.query('user', { username: param });
 				records.then((recordsArray) => {
-					if(records.data.length == 1) {
-						recordsArray.data[0] = records.data;
+					console.log("recordsArray.data.length = " + recordsArray.data.length);
+					if(recordsArray.data.length == 1) {
+						recordsArray.data[0] = recordsArray.data;
 						return recordsArray;
 					} else {
 						return records;
