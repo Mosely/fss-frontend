522d89747717abe312604256bffbb812d8e8f624
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-07-19 22:44:22 -0500

    adding actions to set counselingtopic and ethnicity on adult intake form

--- app/components/adult-intake-form.js
+++ app/components/adult-intake-form.js
@@ -1,4 +1,23 @@
 import Component from '@ember/component';
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+	session: service("session"),
+  store: service("store"),
+
+	actions: {
+		/**
+		 * Will set gender_id to the selected value when the dropdown is updated
+		 *
+		 * @param {string} value The value of the option that is selected
+		 **/
+		updateValue(value) {
+			let target = event.target.classList;
+			if (target.contains("counselingTopic")) {
+				let counselingtopic = this.set("topic", value);
+			} else if (target.contains("ethnicity")) {
+				let ethnicity = this.set("ethnicity", value);
+			}
+		},
+	}
 });
