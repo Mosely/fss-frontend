4f7230733bb59235ae97eab06cf52a48c7371363
Mosely <djhiggs1@gmail.com>
2018-07-21 20:46:30 -0500

    Code Cleanup

--- app/components/add-user-form.js
+++ app/components/add-user-form.js
@@ -12,7 +12,8 @@ export default Component.extend({
      * @param {string} value The value of the option that is selected
      **/
     updateValue(value) {
-      //this.set("gender", value);
+      //NOTE: we'll probably follow this pattern for finding related models whenever we need to 
+      // perform record inserts.
       let store = this.get("store");
       store.find("gender", value).then((model) => {
         this.set("gender", model);
@@ -35,43 +36,21 @@ export default Component.extend({
         "firstName",
         "middleName",
         "lastName",
-        //"gender",
         "dateOfBirth",
         "age"
       );
 
-      //Ember.$.getJSON('http://localhost:9393/artists', function(artists) {
-      //artists.forEach(function(data) {
-      //  artistObjects.pushObject(App.Artist.createRecord(data));
-      //});
-    //});
-
-      //genderModel = store.findRecord("gender", personProps.gender).then((gender) => {
-      //  return gender;
-      //});
-      //personProps.gender = genderModel;
-      //personProps.genderId = personProps.gender;
-
       newPerson = store.createRecord("person", personProps);
       newPerson.set("gender", this.get("gender"));
-      console.table(newPerson);
       userProps = this.getProperties("username", "email", "password");
       newUser = store.createRecord("user", {
         id: newPerson.id,
         userProps
       });
 
-      //newPerson.set("gender", genderModel);
-
       newPerson.save().then(() => {
         return newUser.save();
       });
-
-      // let resolvedPromises = Promise.all([genderModel, newPerson, newUser]).then((values)=> {
-      //   return values;
-      // });
-      // newPerson.set("gender", resolvedPromises);
-      // console.log(resolvedPromises);
     }
   }
 });
