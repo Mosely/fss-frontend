5ab1d94e8d6c1c0ddaceacf9e5db20b36a9b7a0e
Mosely <djhiggs1@gmail.com>
2018-07-27 18:59:39 -0500

    Trying ot Update a User Record

--- app/components/edit-user.js
+++ app/components/edit-user.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    store: service("store"),
+    actions: {
+        update() {
+            //let store = this.get('store');
+            let user = this.get('user');
+            user.save();
+        }
+    }
 });
