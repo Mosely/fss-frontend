669d780fc6ad6a2d534e5682ace55df11632442d
Mosely <djhiggs1@gmail.com>
2018-07-28 22:30:25 -0500

    Change ot fix Reserved Keyword Use

--- app/models/report.js
+++ app/models/report.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   //attributes, if any
   name: DS.attr('string'),
-  type: DS.attr('string'),
+  rtype: DS.attr('string'),
 
   //relationships, if any
   updatedBy: DS.belongsTo('user', { async: false }),
