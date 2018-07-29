a9a99f7aa1981c58ebaf80cb446d9cce89e01af4
mlhomfeld <mlhomfeld@yahoo.com>
2018-07-29 15:49:04 -0500

    Test 3

--- app/routes/reportoutput.js
+++ app/routes/reportoutput.js
@@ -2,5 +2,7 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-
+  model(){
+      return $.getJSON('http://nginx3.pantheon.local/reportoutput/1');
+  }
 });
