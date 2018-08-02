508f34cf857a7a9a68ad3dae9b89d8028296cd65
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-02 07:35:26 -0500

    Adding rest of inputs/trying to update record to no avail, yet.

--- app/components/edit-user.js
+++ app/components/edit-user.js
@@ -1,13 +1,51 @@
 import Component from '@ember/component';
-import { inject as service } from "@ember/service";
 
 export default Component.extend({
-    store: service("store"),
-    actions: {
-        update() {
-            //let store = this.get('store');
-            let user = this.get('user');
-            user.save();
-        }
-    }
+
+	actions: {
+		update(){
+			let updatedPerson,
+				updatedUser,
+        store = this.get("store");
+
+			updatedPerson = this.get('person', this.get("user.person.id"));
+			updatedUser = this.get('user', this.get("user.id"));
+			console.log(this.get("person", 23));
+			// updatedPerson = this.get("person").setProperties({
+			// 	firstName: this.get("user.person.firstName"),
+			// 	middleName: this.get("user.person.middleName"),
+			// 	lastName: this.get("user.person.lastName"),
+			// 	dateOfBirth: this.get("user.person.dateOfBirth"),
+			// });
+			// updatedUser = this.get("user").setProperties({
+			// 	username: this.get("user.person.username"),
+			// 	email: this.get("user.person.email"),
+			// 	gender: this.get("user.person.gender"),
+			// });
+			// updatedPerson.save().then(() => {
+			// 	updatedUser.save();
+			// });
+		},
+		cancle(){
+
+		},
+		destroy() {
+
+		}
+	}
+
 });
+
+// import Component from '@ember/component';
+// import { inject as service } from "@ember/service";
+//
+// export default Component.extend({
+//     store: service("store"),
+//     actions: {
+//         update() {
+//             //let store = this.get('store');
+//             let user = this.get('user');
+//             user.save();
+//         }
+//     }
+// });
