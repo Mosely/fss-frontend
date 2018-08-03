2203d466b2c7d7a2bdcad98b95503f09e8dba232
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-02 21:23:16 -0500

    trying to get full user model and save it

--- app/components/edit-user.js
+++ app/components/edit-user.js
@@ -10,18 +10,9 @@ export default Component.extend({
         store = this.store;
 
 			uid = this.get("user.id");
-			updatedPerson = this.get("person", uid).setProperties({
-				firstName: this.get("user.person.firstName"),
-				middleName: this.get("user.person.middleName"),
-				lastName: this.get("user.person.lastName"),
-				dateOfBirth: this.get("user.person.dateOfBirth"),
-			});
-			updatedUser = this.get("user", uid).setProperties({
-				username: this.get("user.username"),
-				email: this.get("user.email"),
-			});
-			console.log(updatedPerson);
+			updatedUser = this.get("user");
 			console.log(updatedUser);
+			updatedUser.save();
 			// updatedUser = this.get('user', uid).then((user) => {
 			// 	user.set('username', this.get("user.username"));
 			// });
@@ -35,17 +26,3 @@ export default Component.extend({
 	}
 
 });
-
-// import Component from '@ember/component';
-// import { inject as service } from "@ember/service";
-//
-// export default Component.extend({
-//     store: service("store"),
-//     actions: {
-//         update() {
-//             //let store = this.get('store');
-//             let user = this.get('user');
-//             user.save();
-//         }
-//     }
-// });
