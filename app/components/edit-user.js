cb3e15d324ea1cd5991db0869962f1a150457353
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-02 20:32:21 -0500

    trying again

--- app/components/edit-user.js
+++ app/components/edit-user.js
@@ -7,22 +7,22 @@ export default Component.extend({
 			let updatedPerson,
 				updatedUser,
 				uid,
-        store = this.get("store");
+        store = this.store;
 			// updatedPerson = this.get("person").setProperties({
 			// 	firstName: this.get("user.person.firstName"),
 			// 	middleName: this.get("user.person.middleName"),
 			// 	lastName: this.get("user.person.lastName"),
 			// 	dateOfBirth: this.get("user.person.dateOfBirth"),
 			// });
-			// updatedUser = this.get("user").setProperties({
-			// 	username: this.get("user.username"),
-			// 	email: this.get("user.email"),
-			// });
-			uid = this.get("user.id");
-			console.log(uid);
-			updatedUser = store.findRecord('user', uid).then((user) => {
-				user.set('username', this.get("user.username"));
+			updatedUser = this.get("user").setProperties({
+				username: this.get("user.username"),
+				email: this.get("user.email"),
 			});
+			uid = this.get("user.id");
+			console.log(updatedUser);
+			// updatedUser = this.get('user', uid).then((user) => {
+			// 	user.set('username', this.get("user.username"));
+			// });
 			updatedUser.save();
 		},
 		cancle(){
