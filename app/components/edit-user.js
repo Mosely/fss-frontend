a3399ca37b0d9f3b160a1439c8863ed351836fc9
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-03 20:44:50 -0500

    updating user model, trying to update person route.

--- app/components/edit-user.js
+++ app/components/edit-user.js
@@ -1,25 +1,30 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-
 	actions: {
 		update(){
 			let updatedPerson,
-				updatedUser,
-				uid,
-        store = this.store;
+				updatedUser;
+
+			updatedPerson = this.get("user.person");
 			updatedUser = this.get("user");
-			// updatedUser = this.get("user").setProperties({
-			// 	username: this.get("user.username"),
-			// 	email: this.get("user.email"),
+
+			updatedPerson.setProperties({
+				firstName: this.get("user.person.firstName"),
+				middleName: this.get("user.person.middleName"),
+				lastName: this.get("user.person.lastName"),
+				dateOfBirth: this.get("user.person.dateOfBirth")
+			});
+			updatedUser.setProperties({
+				username: this.get("user.username"),
+				email: this.get("user.email")
+			});
+			// saving person rtecord then saving user record.
+			// updatedPerson.save().then(() => {
+			// 	updatedUser.save();
 			// });
-			updatedUser.set("username", this.get("user.username"));
-			updatedUser.set("email", this.get("user.email"));
-			console.log(updatedUser);
+			updatedPerson.save();
 			updatedUser.save();
-			// updatedUser = this.get('user', uid).then((user) => {
-			// 	user.set('username', this.get("user.username"));
-			// });
 		},
 		cancle(){
 
--- app/routes/person.js
+++ app/routes/person.js
@@ -6,10 +6,7 @@ export default Route.extend(AuthenticatedRouteMixin, {
   model(params) {
     let store = this.store;
     return store.findRecord('person', params.id).then(function(person) {
-      //store.findRecord('gender', person.gender_id).then(function(gender) {
-      //    client.set('gender', gender);
-      //  });
-        return person;
-  });
+      return person;
+    });
   }
 });
