44684cd2fdc355027453b0dbd3a07c0ac0cbc5c6
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-04 10:23:36 -0500

    some clean up

--- app/components/edit-user.js
+++ app/components/edit-user.js
@@ -9,20 +9,14 @@ export default Component.extend({
 
 			updatedPerson = this.get("user.person");
 			updatedUser = this.get("user");
-			props = this.getProperties(
-				"user.person.firstName",
-				"user.person.middleName",
-				"user.person.lastName",
-				"user.person.dateOfBirth"
-			);
-			updatedPerson.set("person", props);
+			updatedPerson.setProperties({
+				firstName: this.get("user.person.firstName"),
+				middleName: this.get("user.person.middleName"),
+				lastName: this.get("user.person.lastName"),
+				dateOfBirth: this.get("user.person.dateOfBirth"),
+			});
 			updatedPerson.set("age", null);
-			// updatedPerson.setProperties({
-			// 	firstName: this.get("user.person.firstName"),
-			// 	middleName: this.get("user.person.middleName"),
-			// 	lastName: this.get("user.person.lastName"),
-			// 	dateOfBirth: this.get("user.person.dateOfBirth")
-			// });
+
 			updatedUser.setProperties({
 				username: this.get("user.username"),
 				email: this.get("user.email")
