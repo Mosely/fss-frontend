3d0da6fd7984f866571e88769f3c2ec700466ed4
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-08 21:03:18 -0500

    building more form fields and adding actions

--- app/components/forms/generate-report-form.js
+++ app/components/forms/generate-report-form.js
@@ -1,4 +1,19 @@
 import Component from '@ember/component';
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+	session: service("session"),
+
+	actions: {
+		updateValue(value) {
+			console.log(value);
+			this.set("rtype", value)
+		},
+
+		generateReport() {
+			let props, store = this.get("store");
+			props = this.get('rtype');
+			console.log("Props" + props);
+		}
+	}
 });
