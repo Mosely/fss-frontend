a3119788b6ea394511f46a2b28469e47e5689329
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-09 21:43:06 -0500

    editing template, getting all reportcolumn attributes, creating records

--- app/components/forms/generate-report-form.js
+++ app/components/forms/generate-report-form.js
@@ -1,19 +1,53 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 import { inject as service } from "@ember/service";
+import EmberObject, { computed } from '@ember/object';
 
 export default Component.extend({
-	session: service("session"),
+  session: service("session"),
+  store: service("store"),
 
-	actions: {
-		updateValue(value) {
-			console.log(value);
-			this.set("rtype", value)
-		},
+  actions: {
+    updateValue(value) {
+      this.set("rtype", value);
+    },
 
-		generateReport() {
-			let props, store = this.get("store");
-			props = this.get('rtype');
-			console.log("Props" + props);
-		}
-	}
+    generateReport() {
+      let reportProps,
+        reportColumnProps,
+        reportCriteriaProps,
+        store = this.get("store");
+      reportProps = this.getProperties(
+        "name",
+        "rtype"
+      );
+      const report = store.createRecord('report', reportProps);
+
+      const columns = [
+  			store.createRecord('reportcolumn', {
+  				header: this.get("header1"),
+  				tableName: this.get("tableName1"),
+  				columnName: this.get("columnName1"),
+  				columnOrder: this.get("columnOrder1"),
+  				width: this.get("width1"),
+  			}),
+  			store.createRecord('reportcolumn', {
+          header: this.get("header2"),
+  				tableName: this.get("tableName2"),
+  				columnName: this.get("columnName2"),
+  				columnOrder: this.get("columnOrder2"),
+  				width: this.get("width2"),
+  			}),
+  			store.createRecord('reportcolumn', {
+          header: this.get("header3"),
+  				tableName: this.get("tableName3"),
+  				columnName: this.get("columnName3"),
+  				columnOrder: this.get("columnOrder3"),
+  				width: this.get("width3"),
+  			}),
+  		];
+
+      console.log(report);
+      console.log(columns);
+    }
+  }
 });
--- app/routes/generate-report.js
+++ app/routes/generate-report.js
@@ -1,8 +1,37 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 import { inject as service } from "@ember/service";
+import RSVP from 'rsvp';
 
 export default Route.extend(AuthenticatedRouteMixin, {
 	session: service("session"),
 
+	// model() {
+	// 	const columns = [
+	// 		this.store.createRecord('reportcolumn', {
+	// 			header: [],
+	// 			tableName: [],
+	// 			columnName: [],
+	// 			columnOrder: [],
+	// 			width: [],
+	// 		}),
+	// 		this.store.createRecord('reportcolumn', {
+	// 			header: [],
+	// 			tableName: [],
+	// 			columnName: [],
+	// 			columnOrder: [],
+	// 			width: [],
+	// 		}),
+	// 		this.store.createRecord('reportcolumn', {
+	// 			header: [],
+	// 			tableName: [],
+	// 			columnName: [],
+	// 			columnOrder: [],
+	// 			width: [],
+	// 		}),
+	// 	];
+	// 	return RSVP.hash({
+  //     columns
+  //   });
+	// }
 });
