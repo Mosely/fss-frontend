93ebc9b6d56fd9d7218fe60e607c52496ae5029a
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-13 21:16:53 -0500

    Adding component markup and actions

--- app/components/search-filter.js
+++ app/components/search-filter.js
@@ -1,4 +1,35 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+	classNames: ["search-filter"],
+  value: "",
+
+	/**
+   * Initial hook displaying items calling the filter action with an empty
+   * value.
+   **/
+  init() {
+    this._super(...arguments);
+    //send initial listings calling filter action with an empty value
+    // this.get("filter")("").then(results => this.set("results", results));
+    this.get('filter')('').then((allResults) => {
+      this.set('results', allResults.results);
+    });
+  },
+
+	actions: {
+    /**
+     * Applies the search term filter to the list of users/patients.
+     * Passing 'results' to the {{yield}}
+     **/
+    handleFilter() {
+      let filterInputValue = this.get("value");
+      let filterAction = this.get("filter");
+      filterAction(filterInputValue).then((filterResults) => {
+        if (filterResults.query == this.get('value')) {
+          this.set("results", filterResults.results);
+        }
+      });
+    }
+  }
 });
--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,8 +1,6 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-	userToDelete: null,
-
 	actions: {
 		filterByName(param) {
 			var store = this.get('store');
