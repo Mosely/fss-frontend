3452e73c736e2d1993e844e9dae407c9fa052dc8
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-18 18:03:56 -0500

    fixing logo and adding console lines for full token

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -25,31 +25,31 @@ export default Controller.extend({
         authenticator = "authenticator:oauth2";
 
       //calling authenticate method to authenticate user
-      session
-        .authenticate(authenticator, credentials)
-        .then((users) => {
-          // parseBase64 just converts the base64 encoded string to a javascript object
-          let t = parseBase64(this.get('session.data.authenticated.access_token'));
-          console.log("Test "+t['sub']);
-          // 'sub' is the user's id, as per the JWT draft specification
-          this.store.findRecord('user', t['sub']).then((user) => {
-          // we can store the current user in the ember-simple-auth session
-          this.set('currentUser', user);
+      session.authenticate(authenticator, credentials).then((users) => {
+        console.log("Hello");
+        // parseBase64 just converts the base64 encoded string to a javascript object
+        let t = parseBase64(this.get('session.data.authenticated.access_token'));
+        console.log("Test "+t['sub']);
+        console.log(t);
+        console.table(t);
+        // 'sub' is the user's id, as per the JWT draft specification
+        this.store.findRecord('user', t['sub']).then((user) => {
+        // we can store the current user in the ember-simple-auth session
+        this.set('currentUser', user);
         });
-          //this.store.queryRecord('user', {}).then((user) => {
-          //this.store.findRecord('user', id).then((user) => {
-          //  session.set('currentUser', user);
-          //});
-        })
-        .catch(reason => {
-          if (reason.responseJSON) {
-            RSVP.hash({
-              errors: reason.responseJSON.message
-            }).then((error) => {
-              this.set("errorMessage", error.errors || reason);
-            });
-          }
-        });
+        //this.store.queryRecord('user', {}).then((user) => {
+        //this.store.findRecord('user', id).then((user) => {
+        //  session.set('currentUser', user);
+        //});
+      }).catch(reason => {
+        if (reason.responseJSON) {
+          RSVP.hash({
+            errors: reason.responseJSON.message
+          }).then((error) => {
+            this.set("errorMessage", error.errors || reason);
+          });
+        }
+      });
     },
     handleInput() {
       //variables for the username and password field to animate the label
