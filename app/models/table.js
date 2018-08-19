0c467191e35573efbb5340f2ce2f46f71acf10bd
JimmyJimmbles <jaypjonesofficial@gmail.com>
2018-08-19 16:50:35 -0500

    Editing table routes and models

--- app/models/table.js
+++ app/models/table.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   //attributes, if any
-
+	tablesInFss: DS.attr('string'),
 
   //relationships, if any
 
--- app/models/tablecolumn.js
+++ app/models/tablecolumn.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   //attributes, if any
-	
+	tableColumn: DS.attr('string'),
 
   //relationships, if any
 
