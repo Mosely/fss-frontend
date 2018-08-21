76a4068e010b7291fd9daf5279606c8b5708824d
Jay Jones <jaypjonesofficial@gmail.com>
2018-08-20 19:50:07 -0500

    adding actions to set values for fileds

--- app/components/forms/adult-intake-form.js
+++ app/components/forms/adult-intake-form.js
@@ -3,27 +3,26 @@ import { inject as service } from "@ember/service";
 
 export default Component.extend({
 	session: service("session"),
+	router: service(),
   store: service("store"),
 
 	actions: {
-		/**
-		 * Will set gender_id to the selected value when the dropdown is updated
-		 *
-		 * @param {string} value The value of the option that is selected
-		 **/
-		 updateValue(value) {
- 			let props, store = this.get("store");
-			console.log(value);
- 			// store.find("counselingtopic", value).then(model => {
-      //    this.set("counselingtopic", model);
- 			// 	console.log(model.id);
- 			// 	console.log(this.get("counselingtopic"));
-      //  });
- 		},
+	 updateEthnicity(value) {
+ 			let store = this.get("store");
+			store.find("ethnicity", value).then((model) => {
+				this.set("ethnicity", model);
+			});
+		},
+		updateTopics(value) {
+			let store = this.get("store");
+			// store.find("counselingtopic", value).then((model) => {
+			// 	this.set("counselingtopic", model);
+			// });
+			console.table(value);
 
+ 		},
 		triggerSave() {
-			console.log(this.getProperties("firstName"));
-			console.log(this.getProperties('selectedCities'));
+			console.log(this.get("ethnicity"));
 			console.log(this.getProperties('selectedStates'));
 		}
 	}
