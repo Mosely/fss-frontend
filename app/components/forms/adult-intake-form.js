95ce2420bad10e5609fbca1d08d17c6eae13d212
Jay Jones <jaypjonesofficial@gmail.com>
2018-08-21 00:27:28 -0500

    typos

--- app/components/forms/adult-intake-form.js
+++ app/components/forms/adult-intake-form.js
@@ -4,11 +4,11 @@ import { inject as service } from "@ember/service";
 export default Component.extend({
 	session: service("session"),
 	router: service(),
-  store: service("store"),
+	store: service("store"),
 
 	actions: {
-	 updateEthnicity(value) {
- 			let store = this.get("store");
+		updateEthnicity(value) {
+			let store = this.get("store");
 			store.find("ethnicity", value).then((model) => {
 				this.set("ethnicity", model);
 			});
@@ -18,14 +18,13 @@ export default Component.extend({
 			this.set("selectedTopics", newSelection);
 			topics = this.get("selectedTopics");
 			this.set("counselingtopic", topics);
-
- 		},
+		},
 		updateDrugUse(newSelection, value) {
 			let drugs, store = this.get("store");
 			this.set("selectedDrugs", newSelection);
 			drugs = this.get("selectedDrugs");
 			this.set("druguse", drugs);
- 		},
+		},
 		triggerSave() {
 			console.log(this.get("selectedTopics"));
 			console.log(this.get("selectedDrugs"));
