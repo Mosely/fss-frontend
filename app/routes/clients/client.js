c795c8df99b69cebdebe520edd5eb8d95528d694
Jay Jones <jaypjonesofficial@gmail.com>
2018-09-01 20:04:44 -0500

    Displaying client details, removing unnecessary files

--- app/routes/clients/client.js
+++ app/routes/clients/client.js
@@ -5,9 +5,10 @@ export default Route.extend(AuthenticatedRouteMixin, {
     model(params) {
         let store = this.store;
         return this.store.findRecord('client', params.id).then(function(client) {
-            store.findRecord('person', client.id).then(function(person) {
-                client.set('person', person);
-              });
+          store.findRecord('person', client.id).then(function(person) {
+              client.set('person', person);
+            });
+          return client;
         });
     }
 });
