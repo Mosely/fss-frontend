d29ea9085f2df9ad016da2c96156dec44e202149
Jay Jones <jaypjonesofficial@gmail.com>
2018-09-09 22:27:53 -0500

    Displaying current logged in users details

--- app/routes/user-profile.js
+++ app/routes/user-profile.js
@@ -1,16 +1,23 @@
-import Route from '@ember/routing/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import { inject as service } from '@ember/service';
+import Route from "@ember/routing/route";
+import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
+import { inject as service } from "@ember/service";
 
 export default Route.extend(AuthenticatedRouteMixin, {
-	session: service("session"),
-	currentUser: service("current-user"),
+  session: service("session"),
+  currentUser: service("current-user"),
 
-	model() {
-		let store = this.store, current_user = this.get('session.data.authenticated.current_user');
-		console.log("currentUser " + current_user);
-		return store.findRecord('user', current_user, { reload: true }).then(function(user) {
-			user.set('currentUser', user);
-		});
-	}
+  model() {
+    let store = this.store,
+      current_user = this.get("session.data.authenticated.current_user");
+    console.log("currentUser " + current_user);
+    return store
+      .findRecord("user", current_user, { reload: true })
+      .then(function(user) {
+        user.set("currentUser", user);
+        store.findRecord("person", user.id).then(function(person) {
+          user.set("person", person);
+        });
+        return user;
+      });
+  }
 });
