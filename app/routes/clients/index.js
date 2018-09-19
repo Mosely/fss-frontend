5fbf96fe4299af7c00934e948d4ee4aa4e24b794
Jay Jones <jaypjonesofficial@gmail.com>
2018-09-18 20:56:32 -0500

    setting gender in client route, displaying gender in client list

--- app/routes/clients/index.js
+++ app/routes/clients/index.js
@@ -1,17 +1,24 @@
-import Route from '@ember/routing/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import Route from "@ember/routing/route";
+import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
 export default Route.extend(AuthenticatedRouteMixin, {
   model() {
-   let store = this.store;
-   return store.findAll('client').then(function(primaryModels){
-     // Modify the following to pull in any related models as needed
-     primaryModels.forEach(primaryModel => {
-       store.findRecord('person', primaryModel.id).then(function(secondaryModel) {
-         primaryModel.set('person', secondaryModel);
-       });
-     });
-     return primaryModels;
-   });
+    let store = this.store;
+    return store.findAll("client", { reload: true }).then(function(clients) {
+      // Modify the following to pull in any related models as needed
+      clients.forEach(client => {
+        store
+          .findRecord("person", client.id, { reload: true })
+          .then(function(person) {
+            store
+              .findRecord("gender", person.get("genderId"), { reload: true })
+              .then(function(gender) {
+                person.set("gender", gender);
+              });
+            client.set("person", person);
+          });
+      });
+      return clients;
+    });
   }
 });
--- app/routes/users/index.js
+++ app/routes/users/index.js
@@ -1,20 +1,26 @@
-import Route from '@ember/routing/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import Route from "@ember/routing/route";
+import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
 export default Route.extend(AuthenticatedRouteMixin, {
-   model() {
+  model() {
     let store = this.store;
-    return store.findAll('user', { reload: true }).then(function(users){
+    return store.findAll("user", { reload: true }).then(function(users) {
       users.forEach(user => {
-        store.findRecord('person', user.id, { reload: true }).then(function(person) {
-          Object.keys(person.toJSON()).forEach(function(prop) { console.log(prop + " " + person.get(prop)); } )
-          store.findRecord('gender', person.get('genderId'), { reload: true }).then(function(gender) {
-            person.set('gender', gender);
+        store
+          .findRecord("person", user.id, { reload: true })
+          .then(function(person) {
+            Object.keys(person.toJSON()).forEach(function(prop) {
+              console.log(prop + " " + person.get(prop));
+            });
+            store
+              .findRecord("gender", person.get("genderId"), { reload: true })
+              .then(function(gender) {
+                person.set("gender", gender);
+              });
+            user.set("person", person);
           });
-          user.set('person', person);
-        });
       });
       return users;
     });
-   }
+  }
 });
