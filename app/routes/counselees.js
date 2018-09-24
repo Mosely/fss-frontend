d9d5736cad453a4558c380e2efb22be9025eba75
Jay Jones <jaypjonesofficial@gmail.com>
2018-09-23 20:37:47 -0500

    Editing counselees route

--- app/routes/counselees.js
+++ app/routes/counselees.js
@@ -1,20 +1,20 @@
-import Route from '@ember/routing/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import Route from "@ember/routing/route";
+import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
 export default Route.extend(AuthenticatedRouteMixin, {
-   model() {
+  model() {
     let store = this.store;
-    return store.findAll('counselee').then(function(primaryModels){
+    return store.findAll("counselee").then(function(counselees) {
       // Modify the following to pull in any related models as needed
-      primaryModels.forEach(primaryModel => {
-        store.findRecord('client', primaryModel.id).then(function(secondaryModel) {
-          primaryModel.set('client', secondaryModel);
+      counselees.forEach(counselee => {
+        store.findRecord("client", counselee.id).then(function(client) {
+          counselee.set("client", client);
         });
-        store.findRecord('person', primaryModel.id).then(function(secondaryModel) {
-          primaryModel.set('person', secondaryModel);
+        store.findRecord("person", counselee.id).then(function(person) {
+          counselee.set("person", person);
         });
       });
-      return primaryModels;
+      return counselees;
     });
-   }
+  }
 });
