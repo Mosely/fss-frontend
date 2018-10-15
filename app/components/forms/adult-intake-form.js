c7d9dd38774a70f0baa098276f7dd171a1cfa6c0
Jay Jones <jaypjonesofficial@gmail.com>
2018-10-14 22:27:30 -0500

    saving client record, beginning to save counselee record, trouble setting hasBeenAbused value with the toggle radio

--- app/components/forms/adult-intake-form.js
+++ app/components/forms/adult-intake-form.js
@@ -7,6 +7,19 @@ export default Component.extend({
   store: service("store"),
 
   actions: {
+    ifYesToggle(value) {
+      let store = this.get("store");
+      let parentElem = document.getElementById(event.target.parentElement.id);
+      let targetElem = document.getElementById(parentElem.dataset.toggle);
+      console.log(value);
+      if (value == "true" && targetElem.classList.contains("hidden")) {
+        targetElem.classList.remove("hidden");
+        targetElem.classList.add("active");
+      } else {
+        targetElem.classList.remove("active");
+        targetElem.classList.add("hidden");
+      }
+    },
     updateEthnicity(value) {
       let store = this.get("store");
       store.find("ethnicity", value).then(model => {
@@ -37,98 +50,110 @@ export default Component.extend({
       /**
        * Creating and saving person record and setting properties.
        **/
+      console.log(this.get("hasBeenAbused"));
       // Getting person properties
-      personProps = this.getProperties(
-        "firstName",
-        "middleName",
-        "lastName",
-        "dateOfBirth"
-      );
-      // Setting newPerson to newly created record
-      newPerson = store.createRecord("person", personProps);
-      //Saving the newPerson object
-      newPerson.save().then(function(person) {
-        /**
-         * Creating and saving address and personaddress records and
-         * setting properties.
-         **/
-        // Getting address props
-        let addressProps = this.getProperties(
-          "streetNumber",
-          "streetName",
-          "streetSuffix",
-          "apartmentNumber",
-          "zipcode"
-        );
-        // creating newAddress record
-        let newAddress = store.createRecord("address", addressProps);
-        // Saving newAddress then setting properties
-        newAddress.save().then(function(address) {
-          address.set("cityData", this.get("selectedCities"));
-          address.set("stateData", this.get("selectedStates"));
-          address.set("countyData", this.get("selectedCounties"));
-          // Creating personAddress record and setting it to personAddresses
-          let personAddress = store.createRecord("personaddress", {
-            personId: person.get("id"),
-            addressId: address.get("id"),
-            isPrimary: this.get("isPrimaryAddress")
-          });
-          person.set("personAddresses", personAddress);
-        });
-
-        /**
-         * Creating and saving phone and personphone records and
-         * setting properties.
-         **/
-        // getting phone properties
-        let phoneProps = this.getProperties(
-          "areaCode",
-          "phoneNumber",
-          "extension",
-          "phoneType"
-        );
-        // creating newPhone record
-        let newPhone = store.createRecord("phone", phoneProps);
-        // saving newPhone and personPhone record and setting properties
-        newPhone.save().then(function(phone) {
-          let personPhone = store.createRecord("personphone", {
-            personId: person.get("id"),
-            phoneId: phone.get("id"),
-            isPrimary: this.get("isPrimaryPhone"),
-            canCall: this.get("canCall")
-          });
-          person.set("personPhones", personPhone);
-        });
-
-        /**
-         * Creating client record
-         **/
-        // Getting client proerties
-        let clientProps = this.getProperties(
-          "socialSecurityNumber",
-          "placeOfEmployment",
-          "isServiceMemberOrVeteran",
-          "hasFamilyWhoIsServiceMemberOrVeteran",
-          "isReferredByVeteranResourceCenter",
-          "referral"
-        );
-        // creating newClient record
-        let newClient = store.createRecord("client", clientProps);
-        // saving newClient record
-        newClient.save().then(function(client) {
-          client.set("id", person.get("id"));
-          //creating clientEthnicities record
-          let clientEthnicities = store.createRecord("clientethnicity", {
-            clientId: client.get("id"),
-            ethnicityId: this.get("clientEthnicities")
-          });
-          //setting clientEthnicities
-          client.set("clientEthnicities", clientEthnicities);
-          let clientLanguages = store.createRecord("clientlanguage", {});
-        });
-        // returning person object model
-        // return person;
-      });
+      // personProps = this.getProperties(
+      //   "firstName",
+      //   "middleName",
+      //   "lastName",
+      //   "dateOfBirth"
+      // );
+      // // Setting newPerson to newly created record
+      // newPerson = store.createRecord("person", personProps);
+      // //Saving the newPerson object
+      // newPerson.save().then(function(person) {
+      //   /**
+      //    * Creating and saving address and personaddress records and
+      //    * setting properties.
+      //    **/
+      //   // Getting address props
+      //   let addressProps = this.getProperties(
+      //     "streetNumber",
+      //     "streetName",
+      //     "streetSuffix",
+      //     "apartmentNumber",
+      //     "zipcode"
+      //   );
+      //   // creating newAddress record
+      //   let newAddress = store.createRecord("address", addressProps);
+      //   // Saving newAddress then setting properties
+      //   newAddress.save().then(function(address) {
+      //     address.set("cityData", this.get("selectedCities"));
+      //     address.set("stateData", this.get("selectedStates"));
+      //     address.set("countyData", this.get("selectedCounties"));
+      //     // Creating personAddress record and setting it to personAddresses
+      //     let personAddress = store.createRecord("personaddress", {
+      //       personId: person.get("id"),
+      //       addressId: address.get("id"),
+      //       isPrimary: this.get("isPrimaryAddress")
+      //     });
+      //     person.set("personAddresses", personAddress);
+      //   });
+      //
+      //   /**
+      //    * Creating and saving phone and personphone records and
+      //    * setting properties.
+      //    **/
+      //   // getting phone properties
+      //   let phoneProps = this.getProperties(
+      //     "areaCode",
+      //     "phoneNumber",
+      //     "extension",
+      //     "phoneType"
+      //   );
+      //   // creating newPhone record
+      //   let newPhone = store.createRecord("phone", phoneProps);
+      //   // saving newPhone and personPhone record and setting properties
+      //   newPhone.save().then(function(phone) {
+      //     let personPhone = store.createRecord("personphone", {
+      //       personId: person.get("id"),
+      //       phoneId: phone.get("id"),
+      //       isPrimary: this.get("isPrimaryPhone"),
+      //       canCall: this.get("canCall")
+      //     });
+      //     person.set("personPhones", personPhone);
+      //   });
+      //
+      //   /**
+      //    * Creating client record
+      //    **/
+      //   // Getting client proerties
+      //   let clientProps = this.getProperties(
+      //     "socialSecurityNumber",
+      //     "placeOfEmployment",
+      //     "isServiceMemberOrVeteran",
+      //     "hasFamilyWhoIsServiceMemberOrVeteran",
+      //     "isReferredByVeteranResourceCenter",
+      //     "referral"
+      //   );
+      //   // creating newClient record
+      //   let newClient = store.createRecord("client", clientProps);
+      //   // saving newClient record
+      //   newClient.save().then(function(client) {
+      //     client.set("id", person.get("id"));
+      //     //creating clientEthnicities record
+      //     let clientEthnicities = store.createRecord("clientethnicity", {
+      //       clientId: client.get("id"),
+      //       ethnicityId: this.get("clientEthnicities")
+      //     });
+      //     //setting clientEthnicities
+      //     client.set("clientEthnicities", clientEthnicities);
+      //     let clientLanguages = store.createRecord("clientlanguage", {
+      //       clientId: client.get("id"),
+      //       languageId: this.get("clientLanguages"),
+      //       isPrimary: this.get("isPrimaryLanguage")
+      //     });
+      //     client.set("clientLanguages", clientLanguages);
+      //
+      //     /**
+      //      * Creating counselee record
+      //      **/
+      //     let counseleeProps = this.getProperties();
+      //     let newCounselee = store.createRecord("counselee");
+      //   });
+      //   // returning person object model
+      //   // return person;
+      // });
     }
   }
 });
--- app/components/inputs/toggle-radio.js
+++ app/components/inputs/toggle-radio.js
@@ -1,24 +1,31 @@
 import Component from "@ember/component";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+  store: service("store"),
   tagName: "div",
   classNames: ["form-check", "inline-block"],
   label: null,
   idValue: null,
+  groupValue: null,
   attributeBindings: ["data-toggle"],
 
   actions: {
-    ifYesToggle() {
-      let value = event.target.value;
-      let parentElem = document.getElementById(event.target.parentElement.id);
-      let targetElem = document.getElementById(parentElem.dataset.toggle);
-      if (value == "yes" && targetElem.classList.contains("hidden")) {
-        targetElem.classList.remove("hidden");
-        targetElem.classList.add("active");
-      } else {
-        targetElem.classList.remove("active");
-        targetElem.classList.add("hidden");
-      }
-    }
+    // ifYesToggle(value) {
+    //   let store = this.get("store");
+    //   let parentElem = document.getElementById(event.target.parentElement.id);
+    //   let targetElem = document.getElementById(parentElem.dataset.toggle);
+    //   console.log(this.get("groupValue"));
+    //   console.log(value);
+    //   console.log(this);
+    //   this.set(`${this.get("groupValue")}`, value);
+    //   if (value == "true" && targetElem.classList.contains("hidden")) {
+    //     targetElem.classList.remove("hidden");
+    //     targetElem.classList.add("active");
+    //   } else {
+    //     targetElem.classList.remove("active");
+    //     targetElem.classList.add("hidden");
+    //   }
+    // }
   }
 });
